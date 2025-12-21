a321301ada381987fad4a3d76da69e3dc91571c4
Muhammad Hasan <muhammadhsn72@gmail.com>
2025-12-21 14:06:13 +0500

    commit

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema }  = mongoose;
 
 const User = new Schema({
-  receiptnum:{type:String, required: true},
+  receiptNum:{type:String, required: true},
   name: {type:String, required: true },
   email: { type: String , required: true, unique: true },
   password: { type: String, required: true },

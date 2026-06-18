79f1cebd72c3d880f11ac355b7982b446ec3df8d
Muhammad Hasan <muhammadhsn72@gmail.com>
2026-06-18 13:18:40 +0500

    server error resolve

--- index.js
+++ index.js
@@ -20,9 +20,13 @@ app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
+const startServer = async () => {
+    await connectDB();
 
-connectDB();
+}
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+startServer().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 });

f932782f5bb6b90c1c9e7fbb7fbb71fa8e713cee
Muhammad Hasan <muhammadhsn72@gmail.com>
2026-06-19 21:10:49 +0500

    active inactive

--- routes/user.js
+++ routes/user.js
@@ -125,4 +125,18 @@ router.put("/updateUser", fetchuser, async (req, res) => {
   }
 });
 
+router.put(`/updateUser/:id`, fetchuser, async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+
 module.exports = router;

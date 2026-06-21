096b3dfe50c89bb81644711942816dd361ffa17b
Muhammad Hasan <muhammadhsn72@gmail.com>
2026-06-21 17:12:01 +0500

    user schema updated

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const { Schema }  = mongoose;
 
 const User = new Schema({
-  receiptNum:{type:String, required: true},
   name: {type:String, required: true },
   email: { type: String , required: true, unique: true },
   password: { type: String, required: true },
